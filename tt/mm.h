a6620f6528d536bfae40007d37e289ffff994b41
Jiangboshi0320 <sjbo@xauat.end.cn>
2025-06-25 14:59:34 +0800

    第一次提交

---
+++ tt/mm.h
@@ -0,0 +1,7 @@
+#ifndef __MM_H__
+#define __MM_H__
+#include <stdio.h>
+
+int findMaxNum(int num1, int num2);
+
+#endif // __mm_H__
---
+++ tt/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "mm.h"
+
+int main(void)
+{
+    int a = 10;
+    int b = 20;
+    int c = findMaxNum(a, b);
+    printf("%d\n", c);
+}
